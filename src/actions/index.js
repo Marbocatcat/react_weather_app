69bbc2b8fbc21e3e6c9920ff544076a967ecb60b
Marbocatcat <mar.bocatcat@gmail.com>
2017-07-25 19:30:08 -0500

    added axios promise, and worked on redux actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,16 @@
-
+import axios from 'axios';
 
 const api_key = "287de475be37912e73b181c6ef7feb1b";
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${api_key}`;
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  };
+}
