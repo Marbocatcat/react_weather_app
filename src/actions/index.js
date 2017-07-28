1ba8a513ed9b50289c844ff748d0354d7f20229d
Marbocatcat <mar.bocatcat@gmail.com>
2017-07-27 20:38:53 -0500

    worked on reducers & actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,6 +9,8 @@ export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
 
+  // Redux- Promise takes care of request promise ( unravels )
+
   return {
     type: FETCH_WEATHER,
     payload: request,
